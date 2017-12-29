7a135725a2e267b668653dec4a64a249ec4c5147
caok <190211989@qq.com>
2017-12-29 11:12:51 +0800

    3

--- test/java/src/Beersong.java
+++ test/java/src/Beersong.java
@@ -3,18 +3,14 @@ public static void main(String[] args){
     int beerNum = 10;
     String words = "bottles";
     while(beerNum > 0 )
-    {
-        if(beerNum == 1)
-        {
-            words = "bottle";
-        }
-        
+    {        
         System.out.println(beerNum + " " + words + " " + "on the wall");
         System.out.println("take two"+ " " + words + " " + "on the wall");
         beerNum = beerNum -2;
         
         if( beerNum == 0 )
         {
+        	words = "bottle";
             System.out.println("NO" + " " + words + " " + "on the wall");
         }
     }
